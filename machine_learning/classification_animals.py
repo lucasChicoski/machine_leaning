8b25c394e2518d5baeabbd1cd289f3c259c939a1
Lucas Chicoski <lucas.chicoski@outlook.com>
2022-11-13 23:54:40 -0300

    nova funcionalidade

--- machine_learning/classification_animals.py
+++ machine_learning/classification_animals.py
@@ -1,13 +1,16 @@
-from core import Classification #Importação de módulo
+from core import Classification  # Importação de módulo
 
 # Introdução a classificação usando machine learning
+
+# gordinho, perna curta, auau
 
 
 def main():
     classification = Classification(
-        arrayAnimal=[[1, 1, 1], [1, 0, 0], [0, 0, 1]])
+        arrayAnimal=[[1, 1, 1], [1, 0, 0], [0, 0, 1]], marcacao_teste=[-1, 1, -1])
 
     classification.startClassification()
+    classification.calculandoTaxaDeAcertos()
 
 
 if __name__ == "__main__":
--- machine_learning/core.py
+++ machine_learning/core.py
@@ -7,8 +7,9 @@ from sklearn.naive_bayes import MultinomialNB
 # 1 - verdadeiro , 0 - falso
 # 1 - Porco , -1 Cachorro
 
+
 class Classification:
-    def __init__(self, arrayAnimal):
+    def __init__(self, arrayAnimal, marcacao_teste):
         self.porco1 = [1, 1, 0]
         self.porco2 = [1, 1, 0]
         self.porco3 = [1, 1, 0]
@@ -30,6 +31,9 @@ class Classification:
         self.modelo = MultinomialNB()
 
         self.arrayAnimal = arrayAnimal
+
+        self.marcacao_teste = marcacao_teste
+        self.resultado = []
         pass
 
     def trainingClassification(self):
@@ -47,9 +51,27 @@ class Classification:
 
         resultado = self.modelo.predict(misteryAnimals)
 
+        self.resultado = resultado
+
         for index, animal in enumerate(resultado):
             if(animal == -1):
                 print("Animal {}".format(index), "É um Cachorro")
                 pass
             else:
                 print("Animal {}".format(index), "É um Porco")
+                pass
+            pass
+        pass
+
+    def calculandoTaxaDeAcertos(self):
+        diferenca = self.resultado - self.marcacao_teste
+
+        acertos = [d for d in diferenca if d == 0]
+        total_acertos = len(acertos)
+        total_elementos = len(self.arrayAnimal)
+
+        taxa_acertos = 100 * total_acertos/total_elementos
+
+        print(taxa_acertos)
+
+        # Calculando Taxa de Acertos
